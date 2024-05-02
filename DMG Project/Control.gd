4af6dc8f05a921e2dffaf8f9f1d8f5dbaad576e6
DMGTUD <145370309+DMGTUD@users.noreply.github.com>
2024-05-02 14:59:33 +0100

    Lambon

--- DMG Project/Control.gd
+++ DMG Project/Control.gd
@@ -1,9 +1,13 @@
 extends Control
 
+
 var NumFireballs = 1
+var ControlN = NumFireballs 
+
 
 func _on_button_pressed():
 	NumFireballs = randi_range(1,20)
+	ControlN = NumFireballs 
 	$MarginContainer6/Diceroll.text = str(NumFireballs)
 
 
@@ -13,8 +17,19 @@ func _on_h_slider_changed():
 
 func _on_h_slider_value_changed(value):
 	NumFireballs = value
+	ControlN = NumFireballs 
 	$SliderNum.text = str(NumFireballs)
 
 
-#func _on_cast_button_pressed():
+func _on_cast_button_pressed():
+	while ControlN > 0:
+		var scene_to_instance = preload("res://FireballPref.tscn")
+		
+		
+		
+		ControlN=ControlN-1
+	
+	
+	
+	
 	
---
+++ DMG Project/FireballPref.gd
@@ -0,0 +1,7 @@
+extends RigidBody2D
+
+
+
+
+func _on_area_2d_area_entered(area):
+	queue_free()
